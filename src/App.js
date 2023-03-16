62ae261121a92501d740b0bfae879deb7bee8f1c
israel-juarez-dev <israel.juarez.dev@gmail.com>
2023-03-15 19:20:19 -0600

    antdLayout

--- src/App.js
+++ src/App.js
@@ -1,62 +1,18 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
+import { Layout, Space, Avatar, Card } from "antd";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import AntLayout from "./AntLayout";
+const { Header, Footer, Sider, Content } = Layout;
+
+const { Meta } = Card;
 
 function App() {
-  // display is where the numbers and operators will show, on line 45 i have the div where display shows
-  const [display, setDisplay] = useState([])
+  
+return (
+  <AntLayout/>
+)
 
-  const createDigits = () => {
-    const digits = [];
-    for (let i = 0; i < 10; i++) {
-      digits.push(
-        <button onClick={() => addNumber(i)} key={i}>{i}</button>
-      )
-    }
-    return digits;
-  }
-  const addNumber = (i) => {
-    setDisplay(display + i)
-    console.log()
-  }
-  const addAddition = () => {
-    setDisplay(display + "+")
-  }
-  const addSubtraction = () => {
-    setDisplay(display + "-")
-  }
-  const addMultiplication = () => {
-    setDisplay(display + "*")
-  }
-  const addDivision = () => {
-    setDisplay(display + "/")
-  }
-  const addDecimal = () => {
-    setDisplay(display + ".")
-  }
-
-  // This will solve the whole equation and simplyify it
-  const solveEquation = () => {
-
-  }
-
-  return (
-    <div className="App">
-        <div>
-          {display}
-        </div>
-        <div>
-          <button onClick={addAddition}>+</button>
-          <button onClick={addSubtraction}>-</button>
-          <button onClick={addMultiplication}>*</button>
-          <button onClick={addDivision}>÷</button>
-        </div>
-        <div>
-          {createDigits()}
-          <button onClick={addDecimal}>.</button>
-          <button onClick={solveEquation}>=</button>
-        </div>
-    </div>
-  );
 }
 
 export default App;
